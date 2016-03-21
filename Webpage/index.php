d43a9067457413b61b865588e5cafc4ec6855929
imoongom <imoongom@gmail.com>
2016-03-20 23:48:59 -0700

    Fixed index.php

--- Webpage/index.php
+++ Webpage/index.php
@@ -11,11 +11,13 @@ if(isset($_POST['btn-login']))
 {
 	$email = mysql_real_escape_string($_POST['email']);
 	$upass = mysql_real_escape_string($_POST['pass']);
+	$colour = mysql_real_escape_string($_POST['colour']);
 	
 	$email = trim($email);
 	$upass = trim($upass);
+	$colour = trim($colour);
 	
-	$res=mysql_query("SELECT user_id, user_name, user_pass FROM users WHERE user_email='$email'");
+	$res=mysql_query("SELECT user_id, colour, user_name, user_pass FROM users WHERE user_email='$email'");
 	$row=mysql_fetch_array($res);
 	
 	$count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
